f12c85cf686b7748052583bd30717e38b326e24d
Ross Hamilton <rosshamilton.dev@gmail.com>
2016-06-09 22:56:10 +0100

    refactor specs

--- problem2/char_counter_spec.rb
+++ problem2/char_counter_spec.rb
@@ -1,30 +1,27 @@
 require './char_counter'
 
 RSpec.describe CharCounter do
+  let(:char_counter) { CharCounter.new }
+
   context "with valid input" do
     it "Returns the number of characters" do
-      char_counter = CharCounter.new
       expect(char_counter.count_chars("conure")).to eq(6)
     end
 
     it "strips the whitespace" do
-      char_counter = CharCounter.new
       expect(char_counter.count_chars("   conure   ")).to eq(6)
     end
   end
 
-
   context "with invalid input" do
     context "with an empty string" do
       it "raises an argument error" do
-        char_counter = CharCounter.new
         expect { char_counter.count_chars("")  }.to raise_error(ArgumentError)
       end
     end
 
     context "with a non string argument" do
       it "raises an argument error" do
-        char_counter = CharCounter.new
         expect { char_counter.count_chars(123) }.to raise_error(ArgumentError)
       end
     end
